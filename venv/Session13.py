474c9c18402579cd09e03269edf26d08cf3884e5
Ishant Kumar <er.ishant@gmail.com>
2018-10-03 17:23:36 +0530

    Session13 - DB Operations with MySQL

--- venv/Session13.py
+++ venv/Session13.py
@@ -50,3 +50,73 @@ for row in cursor:
 
 
 # print(p1.name,"Updated in DB")
+
+"""
+Persistance:
+	Save the data of Object permanently
+
+	1. File
+
+	2. DataBase
+		2.1 SQL
+			> MySQL
+			  Oracle
+			  SQLServer
+
+		2.2 No-SQL
+			MongoDB
+			FirebaseFirestore
+			Neo4J
+			.
+			..
+
+		RDBMS	
+
+		w3schools.com
+			SQL Intro	
+
+		DataBase: Collection of Tables
+		Table   : Collection of Rows and Cols
+
+		in 1 database we can have multiple tables related to each other as well
+
+		Object/Table
+		Product
+			pid
+			name
+			price
+
+	Python: Structure of Object		
+	class Product:
+    	def __init__(self, pid, name, price):
+	        self.pid = pid
+	        self.name = name
+	        self.price = price
+
+	SQL	
+	create table Product(
+		pid int primary key auto_increment,
+		name varchar(256),
+		price int
+	)	
+
+	1. Insert Data in Table
+	insert into Product values(null,'iPhoneX',70000)
+
+	2. Update Data in Table
+	update product set name = 'Apple iPhone X', price = 80000 where pid = 1
+
+	3. Delete from Table
+	delete from Product where pid = 1
+
+	4. Retrieve Data from Table
+	select * from Product
+	select name, price from Product
+	select * from Product where price > 40000
+
+	Username and Password
+
+	Make Python to save data in Database
+	mysql-connector-python		
+
+"""
